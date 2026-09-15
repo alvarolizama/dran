ca58ee1329000e3f55d71d025541110198fd3e9f
Alvaro Lizama <soy@alvarolizama.com>
2026-09-15 17:03:26 -0600

    fix(config): quita el modificador E inválido de los patrones de live_reload

--- config/dev.exs
+++ config/dev.exs
@@ -59,12 +59,12 @@ config :dran, DranWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/dran_web/router\.ex$"E,
-      ~r"lib/dran_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/dran_web/router\.ex$",
+      ~r"lib/dran_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
